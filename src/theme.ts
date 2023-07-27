ba3fdf1f28bbe011f4aa0bb7ed6a4f17abafaa7e
mak-somniferum <suhjin700@gmail.com>
2023-07-27 10:11:08 +0900

    first commit

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: '#f5f6fa',
+  accentColor: '#9c88ff',
+  cardBgColor: '#1c2026',
+  cardHoverBgColor: '#111111',
+};
