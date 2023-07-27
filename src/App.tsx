eef684d10cbd806a39af892aa7232ac320f2316d
mak-somniferum <suhjin700@gmail.com>
2023-07-27 10:59:23 +0900

    dnd setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
-  return <></>;
+  const onDragEnd = () => {};
+  return (
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div>
+          <Droppable droppableId="droppable-1">
+            {() => (
+              <ul>
+                <Draggable draggableId="first" index={0}>
+                  {() => <li>Hello one</li>}
+                </Draggable>
+                <Draggable draggableId="second" index={1}>
+                  {() => <li>Hello two</li>}
+                </Draggable>
+              </ul>
+            )}
+          </Droppable>
+        </div>
+      </DragDropContext>
+    </>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import { theme } from './theme';
 
 const GlobalStyle = createGlobalStyle`
